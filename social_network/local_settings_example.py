b95a41c2ba2abdaf3d261f2d48c9084409801d6d
M H Tafteh <m.h.tafteh@gmail.com>
2024-02-19 13:40:07 +0330

    some edit

---
+++ social_network/local_settings_example.py
@@ -0,0 +1,15 @@
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = False
+IS_DEVELOPMENT = False
+
+ALLOWED_HOSTS = ['put your web server host name heir']
+
+# SECURITY WARNING: keep the secret key used in production secret!
+# noinspection SpellCheckingInspection
+SECRET_KEY = 'when you create a django app it creates a sekret key for you'
+
+DB_NAME = 'your database name'
+DB_HOST = 'your mysql host'
+DB_PASS = 'your password'
+DB_USER = 'your username'
